8771d86c9c82dc2fc2011d4cc2efed3799894ab1
akshu <akshu@gmail.com>
2026-07-06 23:38:18 +0530

    Add telemetry_daemon.py

---
+++ Secure-Iot Based Remote Telementary Monitoring System for Satellite Ground station using SSH Tunneling/telemetry_daemon.py
@@ -0,0 +1,16 @@
+import time
+from read_telemetry import get_telemetry
+from ccsds_packet import build_ccsds_packet
+
+LOG_FILE = "/home/pi/telemetry_packets.bin"
+APID = 100
+seq = 0
+
+while True:
+    data = get_telemetry()
+    packet = build_ccsds_packet(APID, seq, data)
+    with open(LOG_FILE, "ab") as f:
+        f.write(len(packet).to_bytes(2, "big") + packet)
+    seq = (seq + 1) % 16384
+    time.sleep(5)
+
