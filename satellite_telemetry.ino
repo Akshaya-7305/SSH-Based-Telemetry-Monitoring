8a3fc49abcbfbc031681ad631d806f43f7d4280f
Aishwarya <aishwarya15369@gmail.com>
2026-07-07 22:50:02 +0530

    Updated satellite telemetry code

--- satellite_telemetry.ino
+++ satellite_telemetry.ino
@@ -1,88 +1,64 @@
-import smbus
-import time
-import Adafruit_DHT
-import socket
-import json
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-# ---------------- DHT11 ---------------- #
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+#define DHTPIN D4          // GPIO2 on NodeMCU
+#define DHTTYPE DHT11
 
-# ---------------- MPU6050 ---------------- #
-bus = smbus.SMBus(1)
-MPU_ADDR = 0x68
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_MPU6050 mpu;
 
-# Wake up MPU6050
-bus.write_byte_data(MPU_ADDR, 0x6B, 0)
+void setup() {
 
-def read_raw_data(addr):
-    high = bus.read_byte_data(MPU_ADDR, addr)
-    low = bus.read_byte_data(MPU_ADDR, addr + 1)
+  Serial.begin(115200);
 
-    value = (high << 8) | low
+  Wire.begin();
 
-    if value > 32768:
-        value -= 65536
+  dht.begin();
 
-    return value
+  if (!mpu.begin()) {
+    Serial.println("MPU6050 not found!");
+    while (1);
+  }
 
-# ---------------- Socket ---------------- #
-PC1_IP = "192.168.1.100"
-PC2_IP = "192.168.1.101"
+  Serial.println("Satellite Telemetry Started");
+}
 
-PORT = 5000
+void loop() {
 
-pc1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-pc2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
-try:
-    pc1.connect((PC1_IP, PORT))
-except:
-    print("PC1 Not Connected")
+  sensors_event_t accel, gyro, temp;
 
-try:
-    pc2.connect((PC2_IP, PORT))
-except:
-    print("PC2 Not Connected")
+  mpu.getEvent(&accel, &gyro, &temp);
 
-print("Satellite Telemetry Started")
+  Serial.print("Temperature:");
+  Serial.print(temperature);
+  Serial.print(" C");
 
-while True:
+  Serial.print(" | Humidity:");
+  Serial.print(humidity);
+  Serial.print(" %");
 
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+  Serial.print(" | AccX:");
+  Serial.print(accel.acceleration.x);
 
-    ax = read_raw_data(0x3B) / 16384.0
-    ay = read_raw_data(0x3D) / 16384.0
-    az = read_raw_data(0x3F) / 16384.0
+  Serial.print(" | AccY:");
+  Serial.print(accel.acceleration.y);
 
-    gx = read_raw_data(0x43) / 131.0
-    gy = read_raw_data(0x45) / 131.0
-    gz = read_raw_data(0x47) / 131.0
+  Serial.print(" | AccZ:");
+  Serial.print(accel.acceleration.z);
 
-    telemetry = {
-        "Temperature": round(temperature, 2) if temperature else None,
-        "Humidity": round(humidity, 2) if humidity else None,
-        "Accel_X": round(ax, 2),
-        "Accel_Y": round(ay, 2),
-        "Accel_Z": round(az, 2),
-        "Gyro_X": round(gx, 2),
-        "Gyro_Y": round(gy, 2),
-        "Gyro_Z": round(gz, 2),
-        "Timestamp": time.strftime("%Y-%m-%d %H:%M:%S")
-    }
+  Serial.print(" | GyroX:");
+  Serial.print(gyro.gyro.x);
 
-    data = json.dumps(telemetry)
+  Serial.print(" | GyroY:");
+  Serial.print(gyro.gyro.y);
 
-    print(data)
+  Serial.print(" | GyroZ:");
+  Serial.println(gyro.gyro.z);
 
-    try:
-        pc1.send((data + "\n").encode())
-    except:
-        pass
-
-    try:
-        pc2.send((data + "\n").encode())
-    except:
-        pass
-
-    time.sleep(2)
+  delay(2000);
+}
